8e608c489f21fc33ceb1bdc288491dc9ade030d1
andy yee <cyee@wayfair.com>
2019-01-07 14:15:48 -0500

    add aliases

---
+++ aliases/wayfair-aliases.zsh
@@ -0,0 +1,59 @@
+# WF
+
+tmreset() {
+  tmux select-layout tiled
+}
+
+wfsync() {
+  realsync ~/codebase
+}
+
+wfdebug() {
+  . ~/dev/config/remote_debug.sh >/dev/null
+}
+
+wms_docker_up() {
+  cd ~/docker-sql-server && docker-compose -p dev up
+}
+
+wfs() {
+  (
+    cd ~/dev/; tmux new-session -d -s wfdev -n window
+    for _ in {1..3}
+      do
+        tmux send-keys -t wfdev:window 'tmux split-window -h' Enter
+    done
+    tmux send-keys -t wfdev:window 'tmux send-keys -t 0.0 "tmux select-layout tiled" Enter' Enter
+    tmux send-keys -t wfdev:window 'tmux send-keys -t 0.1 "wfdebug" Enter' Enter 
+    tmux send-keys -t wfdev:window 'tmux send-keys -t 0.2 "wms_docker_up" Enter' Enter
+    tmux send-keys -t wfdev:window 'tmux send-keys -t 0.3 "wfsync" Enter' Enter
+    tmux send-keys -t wfdev:window 'tmux select-pane -t 0.0' Enter
+    tmux attach -t wfdev:window
+  )
+}
+
+wfe() {
+  tmux kill-server
+}
+
+wfdeploy() {
+  current_branch=$(git rev-parse --abbrev-ref HEAD) \
+    && wf sniff \
+    && git checkout master \
+    && git pull \
+    && git checkout $current_branch \
+    && git rebase master \
+    && git rebase -i $(git rev-parse origin/master) \
+    && git push -uf origin $current_branch
+}
+
+alias wfvm='ssh -t webphp0967.dev.bo1.csnzoo.com bash -l'
+alias snippets='cd ~/dev/wf-snippets'
+alias wfsnip='cd ~/dev/wf-snippets'
+alias codebase='cd ~/codebase'
+alias wfcode='cd ~/codebase'
+alias wfres='cd ~/codebase/resources'
+alias wfphp='cd ~/codebase/php'
+alias wfdb='cd ~/codebase/db'
+alias wfand='cd ~/codebase/tangerine-mobile'
+alias wfpost='wf reviewboard post'
