6918a2a917f0b723473bf8bbee73d12302e0f826
Andy Yee <andyych88@gmail.com>
2019-12-15 03:03:14 -0500

    Switch back to bash (#18)
    * add additional block for work dotfiles
    
    * add comments to install script
    
    * clean up
    
    * move things around

--- .bash_profile
+++ .bash_profile
@@ -1,1 +1,7 @@
+# https://superuser.com/a/583502/225931
+if [ -f /etc/profile ]; then
+  PATH=
+  source /etc/profile
+fi
+
 . ~/.bashrc
--- git/install.sh
+++
@@ -1,4 +0,0 @@
-ssh-keygen -t rsa -b 4096 -C $EMAIL
-pbcopy < ~/.ssh/id_rsa.pub
-
-git config --global core.excludesfile ~/.gitignore_global
